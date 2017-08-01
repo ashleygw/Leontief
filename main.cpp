a77440afab58147b2a1700888eedf855c5720464
ashleygw <george.badash@gmail.com>
2017-08-01 15:25:52 -0700

    updating structure

--- main.cpp
+++ main.cpp
@@ -32,6 +32,76 @@ std::vector<std::string> load_all_sectors(std::vector<std::vector<std::string> >
 	}
 	return sectors;
 }
+
+double determinant(int n, std::vector<std::vector<double> > mat, double d = 0)
+{
+	int c, subi, i, j, subj;
+	std::vector<std::vector<double> > submat(mat.size(), std::vector<double>(mat.size(), 1));
+	if (n == 2)
+	{
+		return((mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]));
+	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			subi = 0;
+			for (i = 1; i < n; i++)
+			{
+				subj = 0;
+				for (j = 0; j < n; j++)
+				{
+					if (j == c)
+					{
+						continue;
+					}
+					submat[subi][subj] = mat[i][j];
+					subj++;
+				}
+				subi++;
+			}
+			d = d + (pow(-1, c) * mat[0][c] * determinant(n - 1, submat, d));
+		}
+	}
+	return d;
+}
+
+double build_Leontief_value(std::vector<std::vector<std::string> > db, int index)
+{
+	std::vector<std::vector<double> > ddb(db.size() - 3, std::vector<double>(db.size() - 3,1));
+	for (int i = 0; i < db.size() - 3; ++i)
+	{
+		double Xj = stod(db[db.size() - 1][i+1]);
+		//Divide columns by XJ
+		for (int j = 0; j < db.size() - 3; ++j)
+		{
+			ddb[i][j] = stod(db[i+1][j+1]) / Xj;
+		}
+	}
+	//Subtract Identity Matrix
+	for (int i = 0; i < db.size(); ++i)
+	{
+		for (int j = 0; j < db.size(); ++j)
+		{
+			ddb[i][j] = (i == j) ? 1 - ddb[i][j] : 0 - ddb[i][j];
+		}
+	}
+	//Inverse
+	double dt = determinant(ddb.size(), ddb);
+
+	return 5.5;
+}
+
+std::vector<double> build_Leontief_values(std::vector<std::vector<std::string> > &db)
+{
+	std::vector<double> values;
+	for (int i = 1; i < db.size() - 2; ++i)
+	{
+		values.push_back(build_Leontief_value(db, i));
+	}
+	return values;
+}
+
 int main(int argc, char* argv[])
 {
 	std::fstream file;
@@ -56,5 +126,6 @@ int main(int argc, char* argv[])
 	}
 	std::vector<std::vector<std::string> > db = load_db(file);
 	std::vector<std::string> all_sectors = load_all_sectors(db);
+	std::vector<double> Leontief_values = build_Leontief_values(db);
 }
 
