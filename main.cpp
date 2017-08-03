f61f3b2294e83e55fdf209054fa6ac2c697f64a4
ashleygw <george.badash@gmail.com>
2017-08-02 22:03:02 -0700

    Pre-test
    Produces leontief values for n sized matrices.
    Not confirmed to be correct yet

--- main.cpp
+++ main.cpp
@@ -1,133 +1,12 @@
 //This program produces LSD tables from csvs.
+//Eigen is used for some of the algebra
+//Created by George Ashley
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <sstream>
-
-//lhs and rhs cant be dim 0
-std::vector<std::vector<double> > multi(std::vector<std::vector<double> > &lhs, std::vector<std::vector<double> > &rhs)
-{
-	std::vector<std::vector<double> > res(rhs.size());
-	//make res with correct dim
-	for (int i = 0; i < res.size(); ++i)
-	{
-		res[i] = std::vector<double>(lhs.size());
-	}
-	//do multiplication
-	for (int i = 0; i < lhs.size(); ++i)
-	{
-		for (int j = 0; j < rhs[0].size(); ++j)
-		{
-			for (int k = 0; k < lhs[0].size(); ++k)
-			{
-				res[i][j] += lhs[i][k] * rhs[k][j];
-			}
-		}
-	}
-	return res;
-}
-
-std::vector<std::vector<double> > add(std::vector<std::vector<double> > &lhs, std::vector<std::vector<double> > &rhs)
-{
-	std::vector<std::vector<double> > res = lhs;
-	//do multiplication
-	for (int i = 0; i < lhs.size(); ++i)
-	{
-		for (int j = 0; j < lhs[0].size(); ++j)
-		{
-			res[i][j] += rhs[i][j];
-		}
-	}
-	return res;
-}
-std::vector<std::vector<double> > scale(std::vector<std::vector<double> > &mat, double factor)
-{
-	std::vector<std::vector<double> > res = mat;
-	for (int i = 0; i < res.size(); ++i)
-	{
-		for (int j = 0; j < res[0].size(); ++j)
-		{
-			res[i][j] *= factor;
-		}
-	}
-	return res;
-}
-
-//takes inverse of matrix that is only diagonal elements
-std::vector<std::vector<double> > simple_inverse(std::vector<std::vector<double> > &mat)
-{
-	std::vector<std::vector<double> > res = mat;
-	for (int i = 0; i < res.size(); ++i)
-	{
-		res[i][i] = 1 / res[i][i];
-	}
-	return res;
-}
-double sqr_error(std::vector<std::vector<double> > mat)
-{
-	double res = 0;
-	for (int i = 0; i < mat.size(); ++i)
-	{
-		for (int j = 0; j < mat[0].size(); ++j)
-		{
-			res += mat[i][j] * mat[i][j];
-		}
-	}
-	return res;
-}
-std::vector<std::vector<double> > inverse(std::vector<std::vector<double> > &mat, double epsilon)
-{
-	std::vector<std::vector<double> > B = mat;
-	//B is identity
-	for (int i = 0; i < B.size(); ++i)
-	{
-		for (int j = 0; j < B[0].size(); ++j)
-		{
-			B[i][j] = 0;
-		}
-		B[i][i] = 1;
-	}
-	std::vector<std::vector<double> > res = B;
-	std::vector<std::vector<double> > D = mat;
-	//D is only the diagonal
-	for (int i = 0; i < D.size(); ++i)
-	{
-		for (int j = 0; j < D[0].size(); ++j)
-		{
-			if (i == j)
-			{
-				continue;
-			}
-			D[i][j] = 0;
-		}
-	}
-	//M + D must equal mat
-	std::vector<std::vector<double> > M = add(mat, scale(D, -1));
-	//we only want invers of D from now on
-	D = simple_inverse(D);
-	//negative of D to avoid calling scale ervy loop
-	std::vector<std::vector<double> > nD = scale(D, -1);
-	std::vector<std::vector<double> > n_identity = scale(B, -1);
-	std::vector<std::vector<double> > DB = multi(D,B);
-	int counter = 0;
-	while (1)
-	{
-		++counter;
-		std::vector<std::vector<double> > prev = res;
-		res = add(multi(nD, multi(M, prev)), DB);
-		//end condition
-		if (counter >= 10)
-		{
-			if (sqr_error(add(multi(mat, res), n_identity)) < epsilon)
-			{
-				break;
-			}
-			counter = 0;
-		}
-	}
-	return res;
-}
+#include "Eigen/Dense"
 
 std::vector<std::vector<std::string> > load_db(std::istream &f)
 {
@@ -154,7 +33,7 @@ std::vector<std::string> load_all_sectors(std::vector<std::vector<std::string> >
 }
 
 
-double build_Leontief_value(std::vector<std::vector<std::string> > db, int index)
+double build_Leontief_value(std::vector<std::vector<std::string> > &db, int index)
 {
 	//Size is reduced and data is converted from string to double
 	std::vector<std::vector<double> > ddb(db.size() - 3, std::vector<double>(db.size() - 3,1));
@@ -176,6 +55,10 @@ double build_Leontief_value(std::vector<std::vector<std::string> > db, int index
 			mult_vec.push_back(ddb[index][i]);
 		}
 	}
+	//Store cut matrix in Eigen vector for multiplication of inverse
+	std::vector<double> z = mult_vec;
+	double* point = &z[0];
+	Eigen::Map<Eigen::VectorXd> mult_vector(point, mult_vec.size());
 	//Erase the unused vectors
 	for (int i = 0; i < ddb.size(); ++i)
 	{
@@ -191,18 +74,17 @@ double build_Leontief_value(std::vector<std::vector<std::string> > db, int index
 		}
 		ddb[i][i] += 1;
 	}
-	//Inverse - Thanks to https://github.com/lavagater for the sweet inverse function
-	ddb = inverse(ddb, 0.000000001);
-	//multiply and add
-	double counter = 0;
-	for (int i = 0; i < mult_vec.size(); ++i)
+	//New Eigen matrix for faster inverting and multiplication
+	Eigen::MatrixXd A(ddb.size(),ddb.size());
+	for (int i = 0; i < ddb.size(); ++i)
 	{
-		for (int j = 0; j < mult_vec.size(); ++j)
-		{
-			counter += mult_vec[j] * ddb[j][i];
-		}
+		std::vector<double> v = ddb[i];
+		double* ptr = &v[0];
+		Eigen::Map<Eigen::VectorXd> my_vect(ptr, ddb.size());
+		A.col(i) = my_vect;
 	}
-	return counter + 1;
+	A = A.inverse();
+	return (A*mult_vector).sum() + 1;
 }
 
 std::vector<double> build_Leontief_values(std::vector<std::vector<std::string> > &db)
@@ -222,7 +104,7 @@ void write_LSD(std::string file_in, std::vector<std::string> &all_sectors, std::
 	std::string temp2 = file_in;
 	temp += temp2 + ").csv";
 	file.open(temp.c_str());
-	file << "LSD," << "Filename:  " << file_in << "\n";
+	file << "LSD," << "Filename:  " << file_in << "\n\n";
 	for (int i = 0; i < all_sectors.size(); ++i)
 	{
 		file << all_sectors[i] << "," << Leontief_values[i] << "\n";
@@ -238,6 +120,7 @@ int main(int argc, char* argv[])
 		file_in = argv[1];
 		if (!file.is_open()) {
 			std::cout << "File not found!\n";
+			std::getchar();
 			return 1;
 		}
 
@@ -249,11 +132,20 @@ int main(int argc, char* argv[])
 		file.open(file_in);
 		if (!file.is_open()) {
 			std::cout << "File not found!\n";
+			std::getchar();
 			return 1;
 		}
 	}
+	std::cout << "Loading database...\n";
 	std::vector<std::vector<std::string> > db = load_db(file);
+	std::cout << "Complete!\n";
+	std::cout << "Loading Sector names...\n";
 	std::vector<std::string> all_sectors = load_all_sectors(db);
+	std::cout << "Complete!\n";
+	std::cout << "Building Leontief values...\n";
 	std::vector<double> Leontief_values = build_Leontief_values(db);
+	std::cout << "Complete!\n";
+	std::cout << "Writing to file...\n";
 	write_LSD(file_in, all_sectors, Leontief_values);
+	std::cout << "Finished.";
 }
